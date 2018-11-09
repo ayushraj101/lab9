bb6ac6739550902805f1fa632623a70b7dab3f41
ayushraj101 <42176710+ayushraj101@users.noreply.github.com>
2018-11-09 23:10:36 +0530

    Update lab9-q17.cpp

--- lab9-q17.cpp
+++ lab9-q17.cpp
@@ -9,9 +9,98 @@ Strstr*/
 #include<iostream>
 #include<cstring>
 using namespace std;
+
+int copy(char str1[], char str2[])
+	{
+		str2=str1;
+		cout<< "The 1st string is : "<<str1<< endl;
+		cout<<"Now 2nd string is also : " str2<<endl;
+		return 0;
+	}
+	
+int attach(char str1[], char str2[])
+	{
+		int n=0, a=0;
+		for (char *p=str1; *p!='\0'; p++)
+			{
+				n++;
+			}
+		for (char* p=str2; *p!='\0'; p++ )
+			{
+				str1[n]=str2[a];
+				a++;
+				n++;
+			}
+		cout<<"Now the 1st string is : "<< str1;
+		return 0;
+	}
+
+int myStrLen(char str1[],char str2[]) 
+	{
+		int counter1=0, counter2=0;
+		
+		/*for(char * poi=arr; *poi != '\0' ; poi++)
+			{
+				counter = counter + 1;
+				cout<< counter<<endl; 
+			}*/
+		char * poi1=str1;
+		while (*poi1 != '\0')
+			{
+				counter1 = counter1 + 1;
+				poi1++;
+			}
+		char * poi2=str2;
+		while (*poi2 != '\0')
+			{
+				counter2 = counter2 + 1;
+				poi2++;
+			}
+		
+		cout<<"The size of 1st string is  :"<< counter1<<endl;
+		cout<<"The size of 2nd string is :" <<counter2<<endl;
+	
+		return 0;;
+	}
+	
+int myStrcmp(char str1[], char str2[])
+	{
+		int counter1=0, counter2=0;
+		char * poi1=str1;
+		while (*poi1 != '\0')
+			{
+				counter1 = counter1 + 1;
+				poi1++;
+			}
+		char * poi2=str2;
+		while (*poi2 != '\0')
+			{
+				counter2 = counter2 + 1;
+				poi2++;
+			}
+		if (counter1 > counter2 )
+		cout<< 1;
+		else if (counter1 < counter2)
+		cout<< "-1";
+		else cout<< 0;
+		return 0;
+	}
+
+int MyStrchr(char str[], char x)
+	{
+		
+		for (char*p = str; p!='\0'; p++)
+			{
+				if (*p=x)
+				{ 
+				
+			}
+	}
+
+
 int main()
 	{
-		char str1[]= "copied", str2[]="useless";
+		/*char str1[]= "copied", str2[]="useless";
 		strcpy(str2, str1);
 		cout<<"strcpy : it copys the array"<< endl;
 		cout<< str2<< " " << str1<<endl;
@@ -36,9 +125,13 @@ int main()
 		
 		cout<<"Strchr is used for finding occurrence of a character in a string"<<endl;
 		char str[]="I am Raj. Ayush Raj";
-		cout<< strchr(str, 'a')<<endl;
+		cout<< strchr(str, 'a')<<endl;*/
 		
-		
+		char str1[1000] ;
+		char str2[1000] ;
+		cin >> str1; str2;
+		cout<<copy(str1,str2);
+		cout<<attach(str1,str2);
 		
 		
 		
